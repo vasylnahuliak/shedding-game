192b653f67806fd9dd5beb8b4fcd10f827c4f82e
Vasyl Nahuliak <vasyl.nahuliak@gmail.com>
2026-05-03 17:47:18 +0100

    fix: update Aptabase initialization method in analytics service

--- apps/frontend/src/services/analytics.native.ts
+++ apps/frontend/src/services/analytics.native.ts
@@ -1,9 +1,9 @@
-import Aptabase, { trackEvent } from '@aptabase/react-native';
+import { init, trackEvent } from '@aptabase/react-native';
 
 import { createAnalyticsClient } from './analytics.shared';
 
 export const analytics = createAnalyticsClient({
-  initialize: (appKey) => Aptabase.init(appKey),
+  initialize: (appKey) => init(appKey),
   trackEvent: (eventName, props) => trackEvent(eventName, props),
 });
 
